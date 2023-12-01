b14148144248285321ea27b67fef5e57ed143394
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2023-12-01 16:03:41 +0530

    added firebase

--- lib/dashboard/orders/view/orders_list.dart
+++ lib/dashboard/orders/view/orders_list.dart
@@ -15,106 +15,111 @@ class _OrdersListState extends State<OrdersList> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: ListView.separated(
-        shrinkWrap: true,
-        itemCount: 8,
-        scrollDirection: Axis.vertical,
-        physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) => GestureDetector(
-          onTap: (){
-            Navigator.pushNamed(context, Routes.viewOrderDetailRoute);
-          },
-          child: Container(
-            width: screenSize.width,
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: ShapeDecoration(
-                    image: const DecorationImage(
-                      image: NetworkImage("https://via.placeholder.com/100x100"),
-                      fit: BoxFit.fill,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10,),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        children: [
+          ListView.separated(
+            shrinkWrap: true,
+            itemCount: 8,
+            scrollDirection: Axis.vertical,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: (){
+                Navigator.pushNamed(context, Routes.viewOrderDetailRoute);
+              },
+              child: Container(
+                width: screenSize.width,
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text(
-                      'Order - #7654',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenSize.width/1.7,
-                      child: const Text(
-                        'Aashirwad toor dal and 4+ products',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 212,
-                      child: Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Order value ',
-                              style: TextStyle(
-                                color: Color(0xFF37474F),
-                                fontSize: 14,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            TextSpan(
-                              text: '₹895',
-                              style: TextStyle(
-                                color: Color(0xFF37474F),
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
                     Container(
                       width: 100,
-                      height: 25,
+                      height: 100,
                       decoration: ShapeDecoration(
-                        color: AppColors.primaryColor,
+                        image: const DecorationImage(
+                          image: NetworkImage("https://via.placeholder.com/100x100"),
+                          fit: BoxFit.fill,
+                        ),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
+                          borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      child: const Center(
-                        child: Text("Packing",style: TextStyle(color: Colors.white),),
-                      ),
+                    ),
+                    const SizedBox(width: 10,),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Order - #7654',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          width: screenSize.width/1.7,
+                          child: const Text(
+                            'Aashirwad toor dal and 4+ products',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 212,
+                          child: Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'Order value ',
+                                  style: TextStyle(
+                                    color: Color(0xFF37474F),
+                                    fontSize: 14,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: '₹895',
+                                  style: TextStyle(
+                                    color: Color(0xFF37474F),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 25,
+                          decoration: ShapeDecoration(
+                            color: AppColors.primaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text("Packing",style: TextStyle(color: Colors.white),),
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ), separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
           ),
-        ), separatorBuilder: (BuildContext context, int index) {
-        return const Divider();
-      },
+          SizedBox(height: 150,)
+        ],
       )
     );
   }
